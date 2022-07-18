df523a8adefc7c0050322d1e425bef198b62c1ab
wkddnjs <wkddnjs0416@gmail.com>
2022-07-18 12:58:32 +0900

    파일 확인

--- homework/src/Bus.java
+++ homework/src/Bus.java
@@ -1,5 +1,26 @@
 public class Bus extends Transport {
 
+    //요소
+    private int nowPassengers;
+
+    private int basic_distance = 1;
+
+    private int basic_price = 3000;
+
+    private int distancePer_price = 1000;
+
+    private int amountPrice;
+    // 12.출력 택시 c 누적요금계산용;
+
+    // 아니면 메인문에 인트배열을 만들어서 저장하자.
+    private int totalPrice;
+
+    // 가이드에 버스 클래스에 현재 승객이 있어서 여기서도 상속받지 않고 사용함.
+    private int nowPassengers;
+
+
+
+
    private int nowPassengers;
 
     public int getNowPassengers() {
